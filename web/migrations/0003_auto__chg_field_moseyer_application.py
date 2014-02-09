d74f104f6dc73ef76f388d1341a0a4331fccc2cb
Clara Thomas <clt84@cornell.edu>
2014-02-09 12:21:02 -0500

    changing app to just be url

---
+++ web/migrations/0003_auto__chg_field_moseyer_application.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Moseyer.application'
+        db.alter_column(u'web_moseyer', 'application', self.gf('django.db.models.fields.CharField')(max_length=1024))
+
+    def backwards(self, orm):
+
+        # Changing field 'Moseyer.application'
+        db.alter_column(u'web_moseyer', 'application', self.gf('django.db.models.fields.files.FileField')(max_length=100))
+
+    models = {
+        u'web.about': {
+            'Meta': {'object_name': 'About'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {'max_length': '2044'})
+        },
+        u'web.alumni': {
+            'Meta': {'object_name': 'Alumni'},
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'web.contact': {
+            'Meta': {'object_name': 'Contact'},
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'position': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'web.moseyer': {
+            'Meta': {'object_name': 'Moseyer'},
+            'application': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '1024', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'picture': ('django.db.models.fields.files.ImageField', [], {'max_length': '255', 'blank': 'True'}),
+            'year': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'web.moseyevent': {
+            'Meta': {'object_name': 'MoseyEvent'},
+            'description': ('django.db.models.fields.TextField', [], {'max_length': '2048'}),
+            'end_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'start_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'})
+        },
+        u'web.newsarticle': {
+            'Meta': {'object_name': 'NewsArticle'},
+            'date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {'max_length': '1024'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['web']
