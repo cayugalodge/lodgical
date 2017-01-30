a2435cddec51fc64217a35bf6cd48d6b213f7864
Weston <westonthomasforster@gmail.com>
2017-01-30 01:19:26 -0500

    fixed urls

--- lodgical/urls.py
+++ lodgical/urls.py
@@ -13,7 +13,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^voting.php/', name='voting'),
     url(r'^$', 'web.views.home', name="home"),
     url(r'^about/$', 'web.views.about', name="about"),
     url(r'^alumni/$', 'web.views.alumni', name="alumni"),
